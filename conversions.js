15e6045d8f2fefbdaee97ac35abea8b68acf185f
Dani Llobet <neollob@yahoo.es>
2023-10-01 11:36:27 +0200

    chore: convert to int

--- conversions.js
+++ conversions.js
@@ -7,3 +7,9 @@ const alsoFalse = !!0       // Expected value: false, typeof alsoFalse: 'boolean
 // Convert to string
 
 const val = 5 + ''      // Expected value: '5' -> typeof val: 'string'
+
+
+// Convert to int
+
+const int = '15'
+int = +int              // Expected value: 15 -> typeof int: 'number'
