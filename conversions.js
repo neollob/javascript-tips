c4cf160c9dac0953e31e8794c66e33ebde5d0aa3
Dani Llobet <neollob@yahoo.es>
2023-10-01 11:36:09 +0200

    chore: convert to string

--- conversions.js
+++ conversions.js
@@ -2,3 +2,8 @@
 
 const isTrue = !0           // Expected value: true, typeof isTrue: 'boolean'
 const alsoFalse = !!0       // Expected value: false, typeof alsoFalse: 'boolean'
+
+
+// Convert to string
+
+const val = 5 + ''      // Expected value: '5' -> typeof val: 'string'
