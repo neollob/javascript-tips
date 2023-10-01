01aca7b0cd4176dbaf0633cafad774cde279ed8c
Dani Llobet <neollob@yahoo.es>
2023-10-01 11:35:49 +0200

    chore: convert to boolean

---
+++ conversions.js
@@ -0,0 +1,4 @@
+// Convert to boolean
+
+const isTrue = !0           // Expected value: true, typeof isTrue: 'boolean'
+const alsoFalse = !!0       // Expected value: false, typeof alsoFalse: 'boolean'
