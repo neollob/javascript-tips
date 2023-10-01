42234aed87003f74cebd9ce52f30fb1c2c2c84ab
Dani Llobet <neollob@yahoo.es>
2023-10-01 12:24:33 +0200

    chore: several string methods

--- string-methods.js
+++ string-methods.js
@@ -1,6 +1,23 @@
+const str = "Hello World";
+str.length;                 // 11
+str.charAt(4);              // o
+str.endsWith("d");          // true
+str.includes("World");      // true
+str.indexOf("W");           // 6;
+str.repeat(2);              // "Hello WorldHello World"
+str.replace("World", "all"); // "Hello all"
+str.slice(6, 10);           // "Worl"
+str.split(" ");             // ["Hello World"]
+str.startsWith("Hello");    // true
+str.substring(6, 11);       // "World"
+str.substr(1, 4);           // "ello"
+str.toLowerCase();          // "hello world"
+str.toUpperCase();          // "HELLO WORLD"
+"     Hello World    ".trim(); // "Hello World"
+
 // The + Operator
 
-const str = 'Hello' + ' ' + 'friends'   // Hello friends
+const str0 = 'Hello' + ' ' + 'friends'   // Hello friends
 
 // Array.join()
 
