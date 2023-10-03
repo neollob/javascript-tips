c393fc7f506cb772e082d83d1613f2fcee728dd9
Dani Llobet <neollob@yahoo.es>
2023-10-03 17:21:35 +0200

    chore: html scoll content

---
+++ HTML_scroll_content.js
@@ -0,0 +1,10 @@
+
+// Use the `scrollLeft` property to make an element scroll horizontally:
+
+const box = document.getElementById('box');
+
+// Scroll content vertically
+box.scrollTop +=50;
+
+// Scroll content horizontally
+box.scrollLeft +=50;
