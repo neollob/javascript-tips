5389b7f09eec01e8156bc947d9f2392e9e183897
Dani Llobet <neollob@yahoo.es>
2023-10-04 20:04:24 +0200

    chore: fix data

--- consoles.js
+++ consoles.js
@@ -12,7 +12,7 @@ function Person(firstName, lastName) {
 }
 
 const john = new Person('John', 'Smith')
-const jane = new Person('jane', 'Doe')
+const jane = new Person('Jane', 'Doe')
 const goku = new Person('Son', 'Goku')
 
 console.table([john, jane, goku]) // Returns array table
