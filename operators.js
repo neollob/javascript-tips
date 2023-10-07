b145ff7deba6f7d24904d944ea6828fd70b30cd9
Dani Llobet <neollob@yahoo.es>
2023-10-07 22:28:10 +0200

    chore: add several operators

--- operators.js
+++ operators.js
@@ -31,10 +31,10 @@ x <= 8	    // true
 
 const y = 6
 const z = 3
-    // && and
-    (y < 10 && z > 1)   // is true
-    // || or
-    (y == 5 || z == 5)  // is false
+// && and
+(y < 10 && z > 1)   // is true
+// || or
+(y == 5 || z == 5)  // is false
 // ! not
 !(y == z)           // is true
 
@@ -56,6 +56,37 @@ let voteable = (age < 18) ? "Too young" : "Old enough";
 "2" > "12"	// true	
 "2" == "12"	// false
 
+
+// Logical AND (&&)
+
+// && (logical conjunction) operator for a set of boolean operands will be true if and only if all the operands are true. Otherwise it will be false
+const a = 3;
+const b = -2;
+
+console.log(a > 0 && b > 0); // Expected output: false
+
+/* 
+expressions that can be converted to false are:
+
+false;
+null;
+NaN;
+0;
+empty string ("" or '' or ``);
+undefined. 
+*/
+
+// Logical AND assignment (&&=)
+
+// &&= operator only evaluates the right operand and assigns to the left if the left operand is truthy.
+let aa = 1;
+let bb = 0;
+
+aa &&= 2;
+console.log(aa); // Expected output: 2
+
+bb &&= 2;
+console.log(bb); // Expected output: 0
 
 // The Nullish Coalescing Operator (??)
 
@@ -86,3 +117,15 @@ console.log(muscleCar.speed);
 const car = { type: "Mustang", model: "GT500", color: "Red" };
 // Ask for car name:
 document.getElementById("demo").innerHTML = car?.name;
+
+
+// Logical OR assignment (||=)
+
+// ||= operator only evaluates the right operand and assigns to the left if the left operand is falsy
+const aaa = { duration: 50, title: '' };
+
+aaa.duration ||= 10;
+console.log(aaa.duration); // Expected output: 50
+
+aaa.title ||= 'title is empty.';
+console.log(aaa.title); // Expected output: "title is empty"
