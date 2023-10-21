ac37e8f3c102cbc354513391b1f8a61697645dbb
Dani Llobet <neollob@yahoo.es>
2023-10-21 09:21:59 +0200

    chore: set remove param destructured

--- destructure.js
+++ destructure.js
@@ -8,3 +8,16 @@ const car = {
 const { performance: whp } = car
 
 console.log({ whp }) // Return: {whp: 1020}
+
+
+// Remove property from object
+
+const products = {
+    "1": { "id": "1", "img": "./coffee-mug.png", "title": "Coffee-Mug - Card", "count": 1 },
+    "2": { "id": "2", "img": "./coffee-mug2.png", "title": "Coffee-Mug - Meme", "count": 1 }
+}
+
+const { "1": toDelete, ...rest } = products
+
+console.log(toDelete) // Return: {id: '1', img: './coffee-mug.png', title: 'Coffee-Mug - Card', count: 1}
+console.log(rest) // Return: {"2": { "id": "2", "img": "./coffee-mug2.png", "title": "Coffee-Mug - Meme", "count": 1 }}
