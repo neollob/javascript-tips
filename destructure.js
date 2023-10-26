2fc970c46967790c3906e65d45841f5bbb3ccead
Dani Llobet <neollob@yahoo.es>
2023-10-26 18:28:22 +0200

    chore: destructure remove prop from object

--- destructure.js
+++ destructure.js
@@ -12,6 +12,20 @@ console.log({ whp }) // Return: {whp: 1020}
 
 // Remove property from object
 
+const myCar = {
+    make: 'Ford',
+    model: 'Mustang',
+    year: 1969
+};
+const get=()=>{
+	const {make,...myCarWO}=myCar	
+	return myCarWO;
+}
+console.log(get()); // Return: { model: 'Mustang', year: 1969 }
+
+
+// Remove property from object
+
 const products = {
     "1": { "id": "1", "img": "./coffee-mug.png", "title": "Coffee-Mug - Card", "count": 1 },
     "2": { "id": "2", "img": "./coffee-mug2.png", "title": "Coffee-Mug - Meme", "count": 1 }
