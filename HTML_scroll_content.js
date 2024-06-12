90b9249c81981e3fa502e213badc9bf5764639e8
Dani Llobet <neollob@yahoo.es>
2024-06-12 19:17:52 +0200

    chore: set scroll content

--- HTML_scroll_content.js
+++ HTML_scroll_content.js
@@ -8,3 +8,12 @@ box.scrollTop +=50;
 
 // Scroll content horizontally
 box.scrollLeft +=50;
+
+
+//El mètode scrollIntoView() de la interfície Element desplaça el contingut principal de l'element de forma que l'element sobre el qual s'invoca scrollIntoView()sea visible a l'usuari.
+var element = document.getElementById("box");
+
+element.scrollIntoView();
+element.scrollIntoView(false);
+element.scrollIntoView({ block: "end" });
+element.scrollIntoView({ block: "end", behavior: "smooth" });
