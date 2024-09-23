bf23a7e3ab674a7f3c3b840b43b388c9d57d559e
Dani Llobet <neollob@yahoo.es>
2024-09-23 18:58:55 +0200

    chore: clean code

--- functions.js
+++ functions.js
@@ -3,43 +3,40 @@
 // Function declaration
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
-sum(2, 3) // Expected: 5
-
+sum(2, 3); // Expected: 5
 
 // Function expression
 
 let sum = function (a, b) {
-    return a + b;
-}
+  return a + b;
+};
 
-sum(2, 3) // Expected: 5
-
+sum(2, 3); // Expected: 5
 
 // Arrow function
 
 let sum = (a, b) => {
+  return a + b;
+};
+
+sum(
+  2,
+  3
+)(
+  // Expected: 5
+
+  // IIFE function
+
+  function (a, b) {
     return a + b;
-}
-
-sum(2, 3) // Expected: 5
-
-
-// IIFE function
-
-(function (a, b) {
-    return a + b;
-})(2, 3); // Expected: 5
-
+  }
+)(2, 3); // Expected: 5
 
 // Function constructor
 
-let sum = new Function(
-    'a',
-    'b',
-    'return a + b'
-);
+let sum = new Function('a', 'b', 'return a + b');
 
-sum(2, 3) // Expected: 5
+sum(2, 3); // Expected: 5
