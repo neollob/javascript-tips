390cf0fca90a3b8336f044e240ef2feeb3b82b98
Dani Llobet <neollob@yahoo.es>
2024-09-24 16:13:57 +0200

    chore: clean code

--- HTML_print.js
+++ HTML_print.js
@@ -1,5 +1,3 @@
 // `window.print()` to create a "print page" functionality.
 
-<button onclick="window.print()">
-    Print this page
-</button>
+<button onclick='window.print()'>Print this page</button>;
