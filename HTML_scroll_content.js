406ff9621ed004cd6ae17fa915a78da6b3f0c204
Dani Llobet <neollob@yahoo.es>
2024-09-24 16:14:00 +0200

    chore: clean code

--- HTML_scroll_content.js
+++ HTML_scroll_content.js
@@ -1,19 +1,17 @@
-
 // Use the `scrollLeft` property to make an element scroll horizontally:
 
 const box = document.getElementById('box');
 
 // Scroll content vertically
-box.scrollTop +=50;
+box.scrollTop += 50;
 
 // Scroll content horizontally
-box.scrollLeft +=50;
-
+box.scrollLeft += 50;
 
 //El mètode scrollIntoView() de la interfície Element desplaça el contingut principal de l'element de forma que l'element sobre el qual s'invoca scrollIntoView()sea visible a l'usuari.
-var element = document.getElementById("box");
+var element = document.getElementById('box');
 
 element.scrollIntoView();
 element.scrollIntoView(false);
-element.scrollIntoView({ block: "end" });
-element.scrollIntoView({ block: "end", behavior: "smooth" });
+element.scrollIntoView({ block: 'end' });
+element.scrollIntoView({ block: 'end', behavior: 'smooth' });
