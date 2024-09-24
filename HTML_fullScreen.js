d4c8ff659d66b1cfb2ed1e50114e210c96a9a3a8
Dani Llobet <neollob@yahoo.es>
2024-09-24 16:13:47 +0200

    chore: clean code

--- HTML_fullScreen.js
+++ HTML_fullScreen.js
@@ -1,8 +1,8 @@
 // `Fullscreen API` to open any element in full-screen mode.
 
-const fullScreenButton = document.getElementById('button')
-const element = document.getElementById('someElement')
+const fullScreenButton = document.getElementById('button');
+const element = document.getElementById('someElement');
 
 fullScreenButton.addEventListener('click', () => {
-    element.requestFullscreen()
-})
+  element.requestFullscreen();
+});
