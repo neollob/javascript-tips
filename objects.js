9dd6f63718b1e9bb9b2f633f4f2ab2e59f0804ad
Dani Llobet <neollob@yahoo.es>
2024-09-25 19:46:43 +0200

    chore: clean code

--- objects.js
+++ objects.js
@@ -1,42 +1,42 @@
 // Iterate object properties by entries
 
 Object.entries(shoppingCart).map(([key, product]) => {
-    console.log({ key, product })
-})
+  console.log({ key, product });
+});
 
 // Iterate object properties by keys
-Object.keys(shoppingCart).map((key) => (
-    { ...shoppingCart[key] }
-)).map(product => {
-    console.log({ product })
-})
+Object.keys(shoppingCart)
+  .map((key) => ({ ...shoppingCart[key] }))
+  .map((product) => {
+    console.log({ product });
+  });
 
 // Sum object values
 
-const demoObject = { a: 2, b: 3, c: 1 }
+const demoObject = { a: 2, b: 3, c: 1 };
 const sumValues = (obj) => Object.values(obj).reduce((a, b) => a + b, 0);
-console.log(sumValues(demoObject)) //Returns: 6
+console.log(sumValues(demoObject)); //Returns: 6
 
 // Add property1 & property2 to object
-object = { ...object, property1: value1, property2: value2 }
+object = { ...object, property1: value1, property2: value2 };
 
 // Add all properties from new_object
-object = { ...object, ...new_object }
+object = { ...object, ...new_object };
 
-const employee = { id: 1, name: "Jone Doe", age: 29 }
+const employee = { id: 1, name: 'Jone Doe', age: 29 };
 
 // Clone object
-var employee_copy = { ...employee} // Result: { id: 1, name: "Jone Doe", age: 29 }
+var employee_copy = { ...employee }; // Result: { id: 1, name: "Jone Doe", age: 29 }
 
 // Clone and add property to new object
-var employee_copy = { ...employee, location: {}, id: 130 } // Result: { id: 130, name: "Jone Doe", age: 29, location: {} }
+var employee_copy = { ...employee, location: {}, id: 130 }; // Result: { id: 130, name: "Jone Doe", age: 29, location: {} }
 
 // updated exisiting object with new property
-employee = { ...employee, id: 130 } // Result: { id: 130, name: "Jone Doe", age: 29}
+employee = { ...employee, id: 130 }; // Result: { id: 130, name: "Jone Doe", age: 29}
 
 // Clone object
-Object.assign( employee, { id: 670 }); // Result: { id: 670, name: "Jone Doe", age: 29}
+Object.assign(employee, { id: 670 }); // Result: { id: 670, name: "Jone Doe", age: 29}
 
 var person = { location: {} };
 
-Object.assign( employee, person); // Result: { id: 670, name: "Jone Doe", age: 29, location: {} }
+Object.assign(employee, person); // Result: { id: 670, name: "Jone Doe", age: 29, location: {} }
