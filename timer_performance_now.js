a8ec91361b98a73d60da10a532ddf28c09d88b3e
Dani Llobet <neollob@yahoo.es>
2024-10-31 19:38:10 +0100

    chore: clean code

--- timer_performance_now.js
+++ timer_performance_now.js
@@ -1,16 +1,13 @@
-
 // Start the timer
-
 const startTime = performance.now();
+
 // Example of code to measure
 for (let i = 0; i < 1000; i++) {
-// Simulate some operation
-Math.sqrt(i);
+  // Simulate some operation
+  Math.sqrt(i);
 }
 
- // End the timer
- const endTime = performance.now(); 12
- console.log(
+// End the timer
+const endTime = performance.now();
 
-`Execution time: ${endTime - startTime} milliseconds`
- );
+console.log(`Execution time: ${endTime - startTime} milliseconds`);
