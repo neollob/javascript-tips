98116c11e65ea8998875e4f006c8178b72717559
Dani Llobet <neollob@yahoo.es>
2024-10-31 19:37:37 +0100

    feat: timer performance now

---
+++ timer_performance_now.js
@@ -0,0 +1,16 @@
+
+// Start the timer
+
+const startTime = performance.now();
+// Example of code to measure
+for (let i = 0; i < 1000; i++) {
+// Simulate some operation
+Math.sqrt(i);
+}
+
+ // End the timer
+ const endTime = performance.now(); 12
+ console.log(
+
+`Execution time: ${endTime - startTime} milliseconds`
+ );
